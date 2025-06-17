7c8e6bc93e524e31612da9b19eddd803a22c4c63
CK991357 <41558235+CK991357@users.noreply.github.com>
2025-06-18 06:57:16 +0800

    语音返回速率调整

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -10,7 +10,7 @@ export const CONFIG = {
     // Default audio settings
     AUDIO: {
         SAMPLE_RATE: 16000,
-        OUTPUT_SAMPLE_RATE: 24000,      // If you want to have fun, set this to around 14000 (u certainly will)
+        OUTPUT_SAMPLE_RATE: 16000,      // 修改为 16000，确保与输入采样率一致
         BUFFER_SIZE: 2048,
         CHANNELS: 1
     },
@@ -20,4 +20,4 @@ export const CONFIG = {
     // }
   };
   
-  export default CONFIG; 
+  export default CONFIG;
