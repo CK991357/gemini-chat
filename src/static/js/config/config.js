8964c4d08fd124c3c5b855ac5bfb7861257014d9
CK991357 <41558235+CK991357@users.noreply.github.com>
2025-08-02 10:12:11 +0800

    增加2.5pro推理

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -4,6 +4,11 @@ export const CONFIG = {
         MODEL_NAME: 'models/gemini-2.0-flash-exp', // 默认模型
         AVAILABLE_MODELS: [
             {
+                name: 'models/gemini-2.5-pro',
+                displayName: 'gemini-2.5-pro (HTTP/Thinking)',
+                isWebSocket: false
+            },
+            {
                 name: 'models/gemini-2.0-flash-exp',
                 displayName: 'gemini-2.0-flash-exp (WebSocket)',
                 isWebSocket: true
